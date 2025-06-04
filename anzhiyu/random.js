26a1ce39330da4b0194b521be417f89dbfeea9ab
woodfishhhh <3053932588@qq.com>
2025-06-04 11:43:26 +0800

    Site updated: 2025-06-04 11:43:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/16/E-Connected-Components/","2025/06/03/Java的final/","2025/05/27/Java的Collection集合/","2025/05/27/Java的包装类/","2025/05/27/Java的List集合/","2025/06/03/Java的多态/","2025/05/15/Java的抽象与接口类/","2025/06/03/Java的继承/","2025/05/15/Java的内部类/"];function toRandomPost(){
+var posts=["2025/05/16/E-Connected-Components/","2025/05/27/Java的Collection集合/","2025/05/27/Java的List集合/","2025/06/03/Java的final/","2025/05/15/Java的内部类/","2025/05/27/Java的包装类/","2025/06/03/Java的多态/","2025/05/15/Java的抽象与接口类/","2025/06/03/Java的继承/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
